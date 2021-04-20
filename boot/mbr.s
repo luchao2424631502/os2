51cce179db2b1b5db931598f688dc705530ebfed
luchao2424631502 <luchao2424631502@gmail.com>
2021-04-20 22:50:03 +0800

    mbr中vga

--- boot/mbr.s
+++ boot/mbr.s
@@ -48,9 +48,9 @@ SCRNX equ   0x0ff4  ;分辨率 x
 SCRNY equ   0x0ff6  ;分辨率 y
 VRAM  equ   0x0ff8  ;图像缓冲区开始的地址
 ;开启VGA模式
-    mov al,0x13     ;VGA 320*200*8
-    mov ah,0x00
-    int 0x10
+    ; mov al,0x13     ;VGA 320*200*8
+    ; mov ah,0x00
+    ; int 0x10
 ;graphics:------
 
 ;
